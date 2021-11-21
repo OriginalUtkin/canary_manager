3bd8e2d3e1d7c404060be9c8ad5e31dec3b0ef00
Utkin Kirill <utkinkir@gmail.com>
2021-11-21 12:36:48 +0100

    Introduce release update (#5)

--- canary_hitman/clients/gitlab.py
+++ canary_hitman/clients/gitlab.py
@@ -1,5 +1,5 @@
 import json
-from typing import Dict, Optional
+from typing import Dict, Optional, Union, List
 
 from requests.exceptions import HTTPError
 
@@ -8,7 +8,7 @@ from canary_hitman.models import Commit, CommitterInfo
 
 
 class GitlabClient(BaseClient):
-    def request_api(self, path: str, params: Optional[Dict] = None) -> Dict:
+    def request_api_get(self, path: str, params: Optional[Dict] = None, json_output: bool = True) -> Union[List, Dict]:
 
         if not params:
             params = {}
@@ -17,11 +17,47 @@ class GitlabClient(BaseClient):
 
         response.raise_for_status()
 
+        if json_output:
+            return json.loads(response.text)
+
+        else:
+            return response.text
+
+    def create_merge_request(
+        self,
+        repository_id: Union[str, int],
+        source_branch: str,
+        target_branch: str,
+        title: str,
+    ):
+        response = self.session.post(
+            f"https://gitlab.skypicker.com/api/v4/projects/{repository_id}/merge_requests", json={
+                "source_branch": source_branch,
+                "target_branch": target_branch,
+                "title": title,
+            },
+        )
+
+        response.raise_for_status()
+
+        return json.loads(response.text)
+
+    def update_file(self, project_id: Union[int, str], branch: str, message: str, file_name: str, file_content: str):
+        response = self.session.post(
+            f"https://gitlab.skypicker.com/api/v4/projects/{project_id}/repository/commits", json={
+                "branch": branch,
+                "commit_message": message,
+                "actions": [{"action": "update", "file_path": f"{file_name}", "content": f"{file_content}"}]
+            },
+        )
+
+        response.raise_for_status()
+
         return json.loads(response.text)
 
     def get_commit(self, repository_id: int, commit_sha: str) -> Optional[Commit]:
         try:
-            api_commit: Dict = self.request_api(path=f"/projects/{repository_id}/repository/commits/{commit_sha}")
+            api_commit: Dict = self.request_api_get(path=f"/projects/{repository_id}/repository/commits/{commit_sha}")
 
         except HTTPError as e:
             if e.response.status_code == 404:
@@ -34,3 +70,13 @@ class GitlabClient(BaseClient):
             web_link=api_commit["web_url"],
             committer=CommitterInfo(email=api_commit["committer_email"]),
         )
+
+    def create_branch(self, project_id: int, branch: str, ref: str):
+        self.session.post(
+            f"https://gitlab.skypicker.com/api/v4/projects/{project_id}/repository/branches",
+            params={
+                "branch": branch,
+                "ref": ref
+            }
+        )
+
---
+++ release_update/main.py
@@ -0,0 +1,45 @@
+from canary_hitman.clients.gitlab import GitlabClient
+import os
+import re
+from typing import List, Dict
+
+
+def get_build_job_id(jobs: List[Dict], branch_ref: str) -> int:
+    for job in jobs:
+        if job["ref"] == branch_ref:
+            return job["id"]
+
+
+if __name__ == "__main__":
+    token = os.environ["GITLAB_API_TOKEN"]
+    filename_to_update = os.environ["FILENAME_TO_UPDATE"]
+    build_branch_reference = os.environ["BUILD_BRANCH_REFERENCE"]
+    target_branch_reference = os.environ["TARGET_BRANCH_REFERENCE"]
+
+    project_id_merge_request = int(os.environ["PROJECT_ID_TO_UPDATE"])
+    project_id_build = int(os.environ["PROJECT_ID_TO_BUILD"])
+
+    cli = GitlabClient(token)
+
+    jobs = cli.request_api_get(path=f"/projects/{project_id_build}/jobs")
+    recent_job_id = get_build_job_id(jobs, build_branch_reference)
+    job_trace = cli.request_api_get(path=f"/projects/{project_id_build}/jobs/{recent_job_id}/trace", json_output=False)
+
+    image_hash = re.findall(r"Successfully tagged (registry.skypicker.com:5005[/a-z-]+:[0-9a-z]{40}-chrome)", job_trace)[0]
+
+    branch_name = f"update_docker_to_{image_hash[-1:-41:-1]}"
+
+    cli.create_branch(project_id=project_id_merge_request, branch=branch_name, ref=target_branch_reference)
+    cli.update_file(
+        project_id=project_id_merge_request,
+        branch=branch_name,
+        message="Bump latest automation image",
+        file_name="Automation-Modules-Master-Dockerfile",
+        file_content=f"FROM {image_hash}\n"
+    )
+    cli.create_merge_request(
+        repository_id=project_id_merge_request,
+        source_branch=branch_name,
+        target_branch=target_branch_reference,
+        title=f"Update {filename_to_update}"
+    )
